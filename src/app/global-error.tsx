6db9d19381bf1e394afc0812bbaad2af2d6729ed
root <root@DESKTOP-EAHBLCU.localdomain>
2025-03-17 07:54:55 +0700

    penambahan root dan dashboard

---
+++ src/app/global-error.tsx
@@ -0,0 +1,16 @@
+"use client"; // Error boundaries must be Client Components
+
+export default function GlobalError({
+
+}: {
+    error: Error & { digest?: string };
+}) {
+    return (
+        // global-error must include html and body tags
+        <html>
+            <body>
+                <h2>Global Error</h2>
+            </body>
+        </html>
+    );
+}
---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>______________</div>
+    );
+}
+
+export default Loading;
